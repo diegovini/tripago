b7b40277a5ec0d16fb82297dbf52137b5012efe3
Diego Vinicius <diegovsva@tcu.gov.br>
2023-02-16 19:57:17 -0300

    aula custom hook

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -1,18 +1,28 @@
-import React, { useState } from 'react'
+import React, {  useState } from 'react'
+import './TripList.css'
+import { useFetch } from './hooks/useFetch';
 
-export default function TripList() {
-    const [trips, setTrips] = useState([]);
-
-    console.log(trips);
-
-  fetch('http://localhost:3000/trips')
-    .then(response => response.json())
-    .then(json => setTrips(json));
-
+export default function TripList() {    
+    const [url, setUrl] = useState('http://localhost:3000/trips')
+    console.log(url)    
+    const {data : trips} = useFetch(url);
 
   return (  
-    <>
-    <h2>teste</h2>
-    </>
+    <div className='trip-list'>
+      <h2>Trip List</h2>
+      <ul>
+        {trips?.map(trip => (
+        <li key={trip.id}>
+          <h3>{trip.title}</h3>
+          <p>{trip.price}</p>
+        </li>
+    ))}
+    </ul>
+    <div className='filters'>
+      <button onClick={() => setUrl('http://localhost:3000/trips?loc=europe')}> European Trips</button>
+      <button onClick={() => setUrl('http://localhost:3000/trips')}> All Trips</button>
+
+    </div>
+    </div>
   )
 }
